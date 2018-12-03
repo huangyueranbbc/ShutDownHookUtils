6054357e9e97c62a0c08e357fa6c5ec6987f4824
huangyueranbbc <huangyueranbbc@users.noreply.github.com>
2018-12-03 09:51:18 +0800

    Update ShutdownHookManagerTest.java

--- src/main/java/ShutdownHookManagerTest.java
+++ src/main/java/ShutdownHookManagerTest.java
@@ -1,9 +1,5 @@
 /*******************************************************************************
- * 版权信息：博睿宏远科技发展有限公司
- * Copyright: Copyright (c) 2007博睿宏远科技发展有限公司,Inc.All Rights Reserved.
- *
  * @date 2018-11-07 下午 1:53 
- * @author: <a href=mailto:huangyr@bonree.com>黄跃然</a>
  * @Description:
  ******************************************************************************/
 public class ShutdownHookManagerTest {
